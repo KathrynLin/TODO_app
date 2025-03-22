31c1aaad2a9b403d5c58d57ee78adb3caef857a6
KathrynLin <lfqing@umich.edu>
2025-03-22 17:51:40 -0400

    backend depolyed on Render, change frontend accordingly

--- todo-frontend/src/services/api.js
+++ todo-frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = process.env.REACT_APP_API_URL;
 
 export const register = (userData) => axios.post(`${API_BASE_URL}/auth/register`, userData);
 export const login = (userData) => axios.post(`${API_BASE_URL}/auth/login`, userData);
